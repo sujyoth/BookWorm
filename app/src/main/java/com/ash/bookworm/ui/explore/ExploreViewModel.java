85c4ff7150eaf2581bef60fcf5ab339c74f51fc9
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-02-11 16:53:42 +0530

    Fixed naming

---
+++ app/src/main/java/com/ash/bookworm/ui/explore/ExploreViewModel.java
@@ -0,0 +1,19 @@
+package com.ash.bookworm.ui.explore;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ExploreViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ExploreViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
