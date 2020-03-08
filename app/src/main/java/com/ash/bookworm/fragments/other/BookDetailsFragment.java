e4f26534286f7208d4ec8ab8b0cde131cecd8c0d
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-03-08 16:46:23 +0530

    Added Search Nearby button in BookDetailsFragment

--- app/src/main/java/com/ash/bookworm/fragments/other/BookDetailsFragment.java
+++ app/src/main/java/com/ash/bookworm/fragments/other/BookDetailsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ash.bookworm.R;
+import com.ash.bookworm.fragments.explore.NearbyFragment;
 import com.ash.bookworm.helpers.models.BaseFragment;
 import com.ash.bookworm.helpers.models.Book;
 import com.ash.bookworm.helpers.models.User;
@@ -29,7 +31,7 @@ public class BookDetailsFragment extends BaseFragment {
 
     private ImageView bookImg;
     private TextView bookTitleTv, bookDescTv, authorNameTv;
-    private Button addBtn;
+    private Button addBtn, searchBtn;
 
     private String bookId, bookTitle, imageUrl, authorName;
     private String prevTitle;
@@ -58,6 +60,25 @@ public class BookDetailsFragment extends BaseFragment {
             public void onClick(View view) {
                 FirebaseUtil.addBookToInventory(new Book(bookId, bookTitle, authorName, imageUrl));
                 Snackbar.make(view, bookTitle + " by " + authorName + " added to inventory", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new NearbyFragment();
+
+                Bundle args = new Bundle();
+                args.putString("bookId", bookId);
+                args.putString("bookName", bookTitle);
+                args.putString("authorName", authorName);
+                args.putString("imageUrl", imageUrl);
+                fragment.setArguments(args);
+
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.add(R.id.nav_host_fragment, fragment);
+                transaction.addToBackStack("Explore");
+                transaction.commit();
             }
         });
 
@@ -103,5 +124,6 @@ public class BookDetailsFragment extends BaseFragment {
         authorNameTv = root.findViewById(R.id.tv_author_name);
         bookDescTv = root.findViewById(R.id.tv_book_desc);
         addBtn = root.findViewById(R.id.btn_add);
+        searchBtn = root.findViewById(R.id.btn_search);
     }
 }
